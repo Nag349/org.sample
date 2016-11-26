b767349692d10c25df8d546ba758dd377c49e975
Nag349 <nagu.ud@gmail.com>
2016-11-26 12:03:39 +0530

    firstcommit

---
+++ src/main/java/org/sample/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+public ModelAndView moves(){
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName("ab");
+	modelAndView.addObject("mess", "anything typed");
+	return modelAndView;
+}
+}
